cb7a47ddc0569b7be1feabae3c117ada1fcc6972
Lox7531 <454545565464f@gmail.com>
2024-09-27 17:10:31 +0300

    Add files via upload

---
+++ lab2/2.10.py
@@ -0,0 +1,18 @@
+#в группе учится n студентов. Каждый получил на экзаменах по 4 оценки. Подсчитать число студентов, не имеющих "2" и "3".
+import random
+random.seed(5)
+print(random.randint(1, 13))
+n=int(input())
+k=0
+for i in range(n):
+    print("Введите 1-ую оценку:")
+    x1 = int(input())
+    print("Введите 2-ую оценку:")
+    x2= int(input())
+    print("Введите 3-ую оценку:")
+    x3= int(input())
+    print("Введите 4-ую оценку:")
+    x4= int(input())
+    if x1>3 or x2>3 or x3>3 or x4>3:
+        k+=1
+print(k)
