acb13a14c83710f73a8fdfac68c497ccd342450e
Lox7531 <454545565464f@gmail.com>
2024-10-18 14:01:34 +0300

    Add files via upload

---
+++ КР/task1.py
@@ -0,0 +1,20 @@
+"""
+вычислить при заданном x сумму s = 1+1/x+1/x^2+...+1/x^10.
+"""
+print("Введите x, при этом x не должен быть равен 0")
+x=int(input())
+s=0
+for i in range(11):
+    s+=(1/x)**i
+print("Cумма равна",s)
+"""
+при x=1
+s=0
+for i in range(11):
+    s+=(1/x1)**i
+print("Cумма равна",s)
+if s==11:
+    print("Okay")
+else:
+    print("Its not fine")
+"""
